77b23e2dd77ac8fd77b20f657a909de7ecfd91dd
AugustoHeitor <auuheitor@gmail.com>
2022-01-17 16:11:55 -0200

    Versão 1.0

---
+++ scripts/novidadesTemporizador.js
@@ -0,0 +1,5 @@
+let imagem = document.getElementById('imagem')
+
+setInterval(() => {imagem.src = "imagens/1.png"}, 1000)
+setInterval(() => {imagem.src = "imagens/2.png"}, 2000)
+setInterval(() => {imagem.src = "imagens/3.png"}, 3000)
